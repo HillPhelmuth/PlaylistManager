eac65cdddcf3a95cf3edb4d19a06e557048a574b
HillPhelmuth <46843652+HillPhelmuth@users.noreply.github.com>
2020-05-09 18:45:21 -0500

    Fix Bug on DeleteVideos component

--- PlaylistManager/Pages/DeleteVideos.razor.cs
+++ PlaylistManager/Pages/DeleteVideos.razor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
 using PlaylistManager.Models;
 using PlaylistManager.Shared;
 
@@ -8,21 +9,22 @@ namespace PlaylistManager.Pages
 {
     public class DeleteVideosModel : PlaylistComponentBase
     {
-        public PlaylistModel Playlist { get; set; }
+        [CascadingParameter]
+        public PlaylistModel SelectedPlaylist { get; set; }
         protected List<VideoModel> LocalVideos = new List<VideoModel>();
 
         protected override async Task OnInitializedAsync()
         {
-            LocalVideos = await Database.GetPlaylistVideos(Playlist);
+            LocalVideos = await Database.GetPlaylistVideos(SelectedPlaylist);
         }
         protected async Task DeleteSelected()
         {
             var selectedVideos = LocalVideos.Where(x => x.IsRemoved);
             foreach (var video in selectedVideos)
             {
-                await Database.RemoveVideoFromPlaylist(video, Playlist);
+                await Database.RemoveVideoFromPlaylist(video, SelectedPlaylist);
             }
-            LocalVideos = await Database.GetPlaylistVideos(Playlist);
+            LocalVideos = await Database.GetPlaylistVideos(SelectedPlaylist);
             StateHasChanged();
         }
     }
